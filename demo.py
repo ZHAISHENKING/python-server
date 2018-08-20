a4a70d7d214d8b79eac0d796f560fb40196b83c3
师仁杰 <docker@ultrabear.cn>
2018-08-20 19:37:30 +0800

    加载静态文件

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, Response, make_response
+from flask import Flask, request, Response, make_response, send_from_directory
 from werkzeug.utils import secure_filename
 import os, re
+from time import sleep
 from flask_restful import Api
 from flask import Response
 
@@ -31,45 +32,49 @@ def upload():
         return filename
 
 
-@app.route('/video/<name>/', methods=["GET"])
-def file(name):
-    p = os.getcwd()+'/uploads/' + name
-    f = open(p, 'rb')
-    # 获取文件大小
-    f.seek(0, os.SEEK_END)
-    filesize = f.tell()
-    f.seek(0, os.SEEK_SET)
-    resp = Response(f, mimetype="video/mp4")
-    if request.headers.get("Range"):
-        # 通过Range大小返回不同Content-Range
-        range_value = request.headers["Range"]
-        HTTP_RANGE_HEADER = re.compile(r'bytes=([0-9]+)\-(([0-9]+)?)')
-        m = re.match(HTTP_RANGE_HEADER, range_value)
-        if m:
-            start_str = m.group(1)
-            start = int(start_str)
-            end_str = m.group(2)
-            end = -1
-            if len(end_str) > 0:
-                end = int(end_str)
-            resp.status_code = 206
-            resp.headers["Content-Type"] = "video/mp4"
+# @app.route('/video/<name>/', methods=["GET"])
+# def file(name):
+#     p = os.getcwd()+'/uploads/' + name
+#     f = open(p, 'rb')
+#     # 获取文件大小
+#     f.seek(0, os.SEEK_END)
+#     filesize = f.tell()
+#     f.seek(0, os.SEEK_SET)
+#     resp = Response(f, mimetype="video/mp4")
+#     if request.headers.get("Range"):
+#         # 通过Range大小返回不同Content-Range
+#         range_value = request.headers["Range"]
+#         HTTP_RANGE_HEADER = re.compile(r'bytes=([0-9]+)\-(([0-9]+)?)')
+#         m = re.match(HTTP_RANGE_HEADER, range_value)
+#         if m:
+#             start_str = m.group(1)
+#             start = int(start_str)
+#             end_str = m.group(2)
+#             end = -1
+#             if len(end_str) > 0:
+#                 end = int(end_str)
+#             resp.status_code = 206
+#             resp.headers["Content-Type"] = "video/mp4"
+#
+#             if end == -1:
+#                 resp.headers["Content-Length"] = str(filesize - start)
+#             else:
+#                 resp.headers["Content-Length"] = str(end - start + 1)
+#             resp.headers["Accept-Ranges"] = "bytes"
+#             if end < 0:
+#                 content_range_header_value = "bytes %d-%d/%d" % (start, filesize - 1, filesize)
+#             else:
+#                 content_range_header_value = "bytes %d-%d/%d" % (start, end, filesize)
+#             resp.headers["Content-Range"] = content_range_header_value
+#
+#             resp.headers["Connection"] = "close"
+#
+#         resp.status_code = 200
+#     return resp
 
-            if end == -1:
-                resp.headers["Content-Length"] = str(filesize - start)
-            else:
-                resp.headers["Content-Length"] = str(end - start + 1)
-            resp.headers["Accept-Ranges"] = "bytes"
-            if end < 0:
-                content_range_header_value = "bytes %d-%d/%d" % (start, filesize - 1, filesize)
-            else:
-                content_range_header_value = "bytes %d-%d/%d" % (start, end, filesize)
-            resp.headers["Content-Range"] = content_range_header_value
-
-            resp.headers["Connection"] = "close"
-
-        resp.status_code = 200
-    return resp
+@app.route('/vd/<name>/', methods=['GET'])
+def play_video(name):
+    return send_from_directory('uploads', name)
 
 
 app.run(debug=True)
